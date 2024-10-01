73f48aefdade83ba81d3294504a2a60b0b37a804
lukman <lukman@lukmans-MacBook-Air.local>
2024-10-01 13:36:22 +0800

    coba test baru

--- src/module/edit_pengumuman/ui/editpengumuman.tsx
+++ src/module/edit_pengumuman/ui/editpengumuman.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation';
 import { BiArrowBack } from 'react-icons/bi';
 import { HiMegaphone } from 'react-icons/hi2';
 
+
+
 function EditPengumuman() {
   const router = useRouter();
   return (
